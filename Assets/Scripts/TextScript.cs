2566ec7d74d01aab2dcbeda15465dd8898b7cc4d
FunReal <ivan7855060@gmail.com>
2020-12-25 22:53:35 +0300

    gth

---
+++ Assets/Scripts/TextScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    private int presses = 0;
+    public Text UIText;
+    // public Vector3 ac;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+
+        UIText.text = "Acselleration.x : " + presses;
+        presses++;
+    }
+}
